27d5e34b79fafe33c5331893298153bad864d6a7
Paaaark <65859360+Paaaark@users.noreply.github.com>
2024-01-06 02:38:02 +0000

    Using StaggeredGrid to show the graphs

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:stocker/models/time_series_daily.dart';
 import 'package:stocker/services/api_service.dart';
 import 'package:stocker/widgets/candle_chart.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -30,20 +30,50 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print("Build");
     return SafeArea(
       child: Scaffold(
         body: isLoading
             ? const Text("Still Loading!")
-            : Container(
-                height: 400,
-                margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).cardColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                ),
-                child: CandleChart(timeSeriesDaily: timeSeriesDaily),
+            : StaggeredGrid.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                children: [
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: CandleChart(timeSeriesDaily: timeSeriesDaily),
+                    ),
+                  ),
+                  const StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: SizedBox(
+                      width: 200,
+                      child: Center(
+                        child: Text("Item 2"),
+                      ),
+                    ),
+                  ),
+                  const StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: SizedBox(
+                      width: 200,
+                      child: Center(
+                        child: Text("Item 3"),
+                      ),
+                    ),
+                  )
+                ],
               ),
       ),
     );
